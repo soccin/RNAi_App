bf777047d5ede608ffed637680a3e00fbf7d251c
Nicholas D. Socci <soccin@mskcc.org>
2016-05-14 19:48:43 -0400

    Added cccpipe2 and fastx tools

---
+++ cccpipe2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for FILE in `ls ../fastq/ | sed 's/\.[^\.]*$//'`
+do
+cat ../fastq/$FILE.fastq | ./fastx_trimmer -f 1 -l 22 -Q 33 \
+|./fastq_quality_converter -Q 33 -n \
+|./fastx_collapser \
+|./fastx_reverse_complement \
+|./fasta_formatter -t \
+|tr '-' '\t'  \
+|cut -f 3,2 > ../Results/result-$FILE.txt
+done
+
+#re type "enter"
+
+
+

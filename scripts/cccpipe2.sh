f6fd0e0400ec8450ce22d2a93c2e90bada515859
Nicholas D. Socci <soccin@mskcc.org>
2016-05-15 19:54:45 -0400

    Added all available scripts

--- scripts/cccpipe2.sh
+++ scripts/cccpipe2.sh
@@ -5,10 +5,10 @@ BDIR=$SDIR/../bin
 FILE=$1
 
 cat $FILE \
-    | $BDIR/fastx_trimmer -f 1 -l 22 -Q 33 \
-    | $BDIR/fastq_quality_converter -Q 33 -n \
-    | $BDIR/fastx_collapser \
-    | $BDIR/fastx_reverse_complement \
-    | $BDIR/fasta_formatter -t \
-    | tr '-' '\t'  \
-    | cut -f 3,2
+	| $BDIR/fastx_trimmer -f 1 -l 22 -Q 33 \
+	| $BDIR/fastq_quality_converter -Q 33 -n \
+	| $BDIR/fastx_collapser \
+	| $BDIR/fastx_reverse_complement \
+	| $BDIR/fasta_formatter -t \
+	| tr '-' '\t'  \
+	| cut -f 3,2

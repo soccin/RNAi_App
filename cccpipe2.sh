6dc19dc834a7bdf3c07e306035a2b26d7d7d5410
Nicholas D. Socci <soccin@mskcc.org>
2016-05-14 20:03:16 -0400

    Work on cccpipe2 script

--- cccpipe2.sh
+++ cccpipe2.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
-for FILE in `ls ../fastq/ | sed 's/\.[^\.]*$//'`
-do
-cat ../fastq/$FILE.fastq | ./fastx_trimmer -f 1 -l 22 -Q 33 \
-|./fastq_quality_converter -Q 33 -n \
-|./fastx_collapser \
-|./fastx_reverse_complement \
-|./fasta_formatter -t \
-|tr '-' '\t'  \
-|cut -f 3,2 > ../Results/result-$FILE.txt
+
+SDIR="$( cd "$( dirname "$0" )" && pwd )"
+BDIR=$SDIR/bin
+
+function usage {
+    echo
+    echo "usage: $(basename $0) [FASTQ_DIR]"
+    echo "   Must specify directory with some FASTQ files"
+    echo "   e.g. XXXX.fastq"
+    echo
+    exit
+}
+
+if [ "$#" == "1" ]; then
+    FASTQ_DIR=$1
+else
+    FASTQ_DIR=$PWD
+fi
+
+NUMFILES=$(ls $FASTQ_DIR/*fastq | wc -l | awk '{print $1}')
+if [ "$NUMFILES" == "0" ]; then
+    usage
+    exit
+fi
+
+mkdir -p Results
+
+for FILE in $FASTQ_DIR/*fastq; do
+    BASE=$(basename $FILE | sed 's/.fastq//')
+
+    cat $FILE \
+    | $BDIR/fastx_trimmer -f 1 -l 22 -Q 33 \
+    | $BDIR/fastq_quality_converter -Q 33 -n \
+    | $BDIR/fastx_collapser \
+    | $BDIR/fastx_reverse_complement \
+    | $BDIR/fasta_formatter -t \
+    | tr '-' '\t'  \
+    | cut -f 3,2 >Results/result-${BASE}.txt
+
 done
-
-#re type "enter"
-
-
 

08304bfffd4be9c164246d727a3308482ae12ecc
Nicholas D. Socci <soccin@mskcc.org>
2016-05-15 14:39:54 -0400

    fixed 2645.sh script work in new format

--- 2645.sh
+++ 2645.sh
@@ -1,16 +1,51 @@
 #!/bin/bash
-for FILE in `ls ../fastq/ | sed 's/\.[^\.]*$//'`
-do
-cat ../fastq/$FILE.fastq | ./fastx_trimmer -f 26 -l 45 -Q 33 \
-|./fastq_quality_converter -Q 33 -n \
-|./fastq_quality_filter -Q 33 -p 1 -q 20 \
-|./fastx_collapser \
-|./fasta_formatter -t \
-|tr '-' '\t'  \
-|cut -f 3,2 > ../Results/result-$FILE.txt
+
+#
+# START:PREAMBLE
+#
+
+SDIR="$( cd "$( dirname "$0" )" && pwd )"
+BDIR=$SDIR/bin
+
+function usage {
+    echo
+    echo "usage: $(basename $0) [FASTQ_DIR]"
+    echo "   Must specify directory with some FASTQ files"
+    echo "   e.g. XXXX.fastq"
+    echo
+    exit
+}
+
+if [ "$#" == "1" ]; then
+    FASTQ_DIR=$1
+else
+    FASTQ_DIR=$PWD
+fi
+
+NUMFILES=$(ls $FASTQ_DIR/*fastq | wc -l | awk '{print $1}')
+if [ "$NUMFILES" == "0" ]; then
+    usage
+    exit
+fi
+
+#
+# END:PREAMBLE
+#
+
+STAG=$(basename $0 | sed 's/.sh//')
+mkdir -p Results
+
+for FILE in $FASTQ_DIR/*fastq; do
+    BASE=$(basename $FILE | sed 's/.fastq//')
+
+    cat $FILE \
+	|$BDIR/fastx_trimmer -f 26 -l 45 -Q 33 \
+	|$BDIR/fastq_quality_converter -Q 33 -n \
+	|$BDIR/fastq_quality_filter -Q 33 -p 1 -q 20 \
+	|$BDIR/fastx_collapser \
+	|$BDIR/fasta_formatter -t \
+	|tr '-' '\t'  \
+	|cut -f 3,2 > Results/result--${STAG}--$BASE.txt
+
 done
-
-#re type "enter"
-
-
 
